a51e301f2fdba93077a1b5d901d868b63de837c4
bpaulon <bogdan.paulon@gmail.com>
2016-06-16 17:25:24 +0200

    Create Customer.java

---
+++ jackson-mixin/src/main/java/bcp/spring/jackson/Customer.java
@@ -0,0 +1,15 @@
+package bcp.spring.jackson;
+
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true)
+@lombok.Getter
+@lombok.Setter
+@lombok.ToString
+public class Customer {
+
+	private String id;
+	private String firstName;
+	private String secondName;
+	
+}
