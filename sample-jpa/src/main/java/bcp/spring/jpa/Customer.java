47488c221049db5ff5e837331acaf7d6220a980b
bpaulon <bogdan.paulon@gmail.com>
2016-06-24 16:59:50 +0200

    Create Customer.java

---
+++ sample-jpa/src/main/java/bcp/spring/jpa/Customer.java
@@ -0,0 +1,39 @@
+package bcp.spring.jpa;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Table(name = "CUSTOMER", schema = "SAMPLE_SCHEMA")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+	@Id
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
+	@SequenceGenerator(name = "SEQ_GEN", sequenceName = "CUSTOMER_SEQ", allocationSize = 1)
+	Long id;
+	
+	@Column(name="FIRST_NAME")
+	String firstName;
+	
+	String secondName;
+	
+}
