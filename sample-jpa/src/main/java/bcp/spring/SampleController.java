c93b2f9160600d744e4806f30f9f1ed25416fde0
bpaulon <bogdan.paulon@gmail.com>
2016-06-27 16:41:58 +0200

    Update SampleController.java

--- sample-jpa/src/main/java/bcp/spring/SampleController.java
+++ sample-jpa/src/main/java/bcp/spring/SampleController.java
@@ -1,13 +1,17 @@
 package bcp.spring;
 
+import static org.springframework.data.jpa.domain.Specifications.where;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bcp.spring.jpa.Customer;
 import bcp.spring.jpa.CustomerRepository;
+import static bcp.spring.jpa.CustomerSpecifications.*;
 
 @RestController
 public class SampleController {
@@ -20,7 +24,8 @@ public class SampleController {
 		Customer customer = Customer.builder().firstName("John").secondName("Ripper").build();
 		repository.save(customer);
 
-		List<Customer> customers = repository.findAll();
+		Specification<Customer> hasFirstNameAndSecondName = where(hasFirstName("John")).and(hasSecondName("Ripper"));
+		List<Customer> customers = repository.findAll(hasFirstNameAndSecondName);
 		return customers;
 	}
 	
