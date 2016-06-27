dfdd5c1690f50cce8c45cdc6a8d1310ed6e44a98
bpaulon <bogdan.paulon@gmail.com>
2016-06-27 16:42:57 +0200

    Create CustomerSpecifications.java

---
+++ sample-jpa/src/main/java/bcp/spring/jpa/CustomerSpecifications.java
@@ -0,0 +1,14 @@
+package bcp.spring.jpa;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class CustomerSpecifications {
+
+	public static Specification<Customer> hasFirstName(String name) {
+		return (root, query, cb) -> cb.equal(root.get(Customer_.firstName), name);
+	}
+	
+	public static Specification<Customer> hasSecondName(String name) {
+		return (root, query, cb) -> cb.equal(root.get(Customer_.secondName), name);
+	}
+}
