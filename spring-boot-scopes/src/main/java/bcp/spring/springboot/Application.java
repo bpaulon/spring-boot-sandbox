9a3fd67b498db574f3684d1bf402ad5ec613283b
bogdan_paulon <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>
2021-12-08 16:34:39 +0200

    fixed message

--- spring-boot-scopes/src/main/java/bcp/spring/springboot/Application.java
+++ spring-boot-scopes/src/main/java/bcp/spring/springboot/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	public static void main(String[] args) {
 	  
-	  log.debug(">>> Starting application");
+	  log.debug(">>>> Starting  SpringBoot application");
 		SpringApplication.run(Application.class, args);
 	}
 
